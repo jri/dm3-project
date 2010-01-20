d2aa857525bb368fe54c1375dfec978273e1f7e7
Jörg Richter <jri@deepamehta.de>
2010-01-20 01:35:23 +0100

    Adapted to current DeepaMehta 3 version.

--- vendor/_attachments/script/dm3_project.js
+++ vendor/_attachments/script/dm3_project.js
@@ -1,11 +1,11 @@
 function dm3_project() {
 
-    types["To Do"] = {
+    add_topic_type("To Do", {
         fields: [
             {id: "Summary",  model: {type: "text"}, view: {editor: "single line"},           content: ""},
             {id: "due-date", model: {type: "date"}, view: {label: "Due Date"},               content: ""},
             {id: "Notes",    model: {type: "html"}, view: {editor: "multi line", lines: 20}, content: ""}
         ],
         implementation: "PlainDocument"
-    }
+    })
 }
