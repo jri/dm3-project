c9e8273fdc0e5d01704bc11e9816ac489d985edb
Jörg Richter <jri@deepamehta.de>
2010-02-09 02:54:16 +0100

    Topic type "To Do" got an icon.

--- vendor/_attachments/script/dm3_project.js
+++ vendor/_attachments/script/dm3_project.js
@@ -6,6 +6,9 @@ function dm3_project() {
             {id: "due-date", model: {type: "date"}, view: {label: "Due Date"},               content: ""},
             {id: "Notes",    model: {type: "html"}, view: {editor: "multi line", lines: 20}, content: ""}
         ],
+        view: {
+            icon_src: "vendor/dm3-project/images/shovel.png"
+        },
         implementation: "PlainDocument"
     })
 }
